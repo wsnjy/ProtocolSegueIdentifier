6e9e97f02c96e80aab33d6e3eeb7af15a4ec1381
Wisnu Sanjaya <wisnu@sebangsa.com>
2016-11-21 19:48:44 +0700

    Segue identifier using protocol swift 3

---
+++ ProtocolSegueIdentifier/SegueHandlerType.swift
@@ -0,0 +1,9 @@
+//
+//  SegueHandlerType.swift
+//  ProtocolSegueIdentifier
+//
+//  Created by Wisnu Sanjaya on 11/21/16.
+//  Copyright © 2016 Sanjaya Wisnu. All rights reserved.
+//
+
+import Foundation
